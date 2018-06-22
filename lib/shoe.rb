5071ea11ec7aaa8ca803d11e89924243fd489305
Wendy Calderon <wendycalderon@gmail.com>
2018-06-22 02:41:33 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -13,9 +14,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand 
-    @brand = brand
-    if brand != 
+
     
   end
 end
