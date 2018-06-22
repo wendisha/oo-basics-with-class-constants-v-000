f818e6e376b018189dd6d078f9e53d663a519f53
Wendy Calderon <wendycalderon@gmail.com>
2018-06-22 03:03:11 +0000

    Done.

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -1,4 +1,4 @@
-describe 'Shoe' do 
+midescribe 'Shoe' do 
   describe 'BRANDS' do
     # Think about it! When does the BRANDS constant need to be
     # made aware of the brands of the shoes that are being created?
